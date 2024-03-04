4ba10ac2a4cbc4646f68e4b9978e57435741a8f8
daniel araujo <daniellealuau@gmail.com>
2024-03-04 13:53:17 -0300

    portabilidade

--- 02/questao2.js
+++ 02/questao2.js
@@ -39,91 +39,42 @@ class Triangulo {
 }
 
 function criarTriangulo() {
-    inquirer.prompt([{
-        type: "input",
-        name: "x1",
-        message: "digite o lado x do triangulo 1: "
-    },
-    {
-        type: "input",
-        name: "y1",
-        message: "digite o lado y do triangulo 1: "
-    },
-    {
-        type: "input",
-        name: "z1",
-        message: "digite o lado z do triangulo 1: "
-    },
-    {
-        type: "input",
-        name: "x2",
-        message: "digite o lado x do triangulo 2: "
-    },
-    {
-        type: "input",
-        name: "y2",
-        message: "digite o lado y do triangulo 2: "
-    },
-    {
-        type: "input",
-        name: "z2",
-        message: "digite o lado z do triangulo 2: "
-    },
-    {
-        type: "input",
-        name: "x3",
-        message: "digite o lado x do triangulo 3: "
-    },
-    {
-        type: "input",
-        name: "y3",
-        message: "digite o lado y do triangulo 3: "
-    },
-    {
-        type: "input",
-        name: "z3",
-        message: "digite o lado z do triangulo 3: "
-    },
-    ]).then((answers) => {
-        const triangulo1 = new Triangulo(
-            Number(answers.x1),
-            Number(answers.y1),
-            Number(answers.z1)
-        )
-        const triangulo2 = new Triangulo(
-            Number(answers.x2),
-            Number(answers.y2),
-            Number(answers.z2)
-        )
-        const triangulo3 = new Triangulo(
-            Number(answers.x3),
-            Number(answers.y3),
-            Number(answers.z3)
-        )
-        console.log(
-            `triangulo 1
-          x: ${triangulo1.getX()}
-          y: ${triangulo1.getY()}
-          z: ${triangulo1.getZ()}
-          `)
-        console.log(
-            `triangulo 2
-          x: ${triangulo2.getX()}
-          y: ${triangulo2.getY()}
-          z: ${triangulo2.getZ()}
-          `)
-        console.log(
-            `triangulo 3
-          x: ${triangulo3.getX()}
-          y: ${triangulo3.getY()}
-          z: ${triangulo3.getZ()}
-          `)
-          console.log(
-            `os triangulos sao iguais?
-          ${triangulo1.equals(triangulo2)}
-          `
-          );
-    })
+  inquirer
+  .prompt([
+    { type: "input", name: "x1", message: "digite o lado x do triângulo 1: " },
+    { type: "input", name: "y1", message: "digite o lado y do triângulo 1: " },
+    { type: "input", name: "z1", message: "digite o lado z do triângulo 1: " },
+    { type: "input", name: "x2", message: "digite o lado x do triângulo 2: " },
+    { type: "input", name: "y2", message: "digite o lado y do triângulo 2: " },
+    { type: "input", name: "z2", message: "digite o lado z do triângulo 2: " },
+    { type: "input", name: "x3", message: "digite o lado x do triângulo 3: " },
+    { type: "input", name: "y3", message: "digite o lado y do triângulo 3: " },
+    { type: "input", name: "z3", message: "digite o lado z do triângulo 3: " },
+  ])
+    .then((answers) => {
+      const triangulo1 = new Triangulo(
+        Number(answers.x1),
+        Number(answers.y1),
+        Number(answers.z1)
+      );
+      const triangulo2 = new Triangulo(
+        Number(answers.x2),
+        Number(answers.y2),
+        Number(answers.z2)
+      );
+      const triangulo3 = new Triangulo(
+        Number(answers.x3),
+        Number(answers.y3),
+        Number(answers.z3)
+      );
+      console.log(`triângulo 1 
+      x: ${triangulo1.getX()} y: ${triangulo1.getY()} z: ${triangulo1.getZ()}`);
+      console.log(`triângulo 2 
+      x: ${triangulo2.getX()} y: ${triangulo2.getY()} z: ${triangulo2.getZ()}`);
+      console.log(`triângulo 3 
+      x: ${triangulo3.getX()} y: ${triangulo3.getY()} z: ${triangulo3.getZ()}`);
+      console.log(`os triângulos são iguais? ${triangulo1.equals(triangulo2)}`);
+    });
 }
 
-criarTriangulo()
+criarTriangulo();
