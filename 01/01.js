c47bfb56b1a09085f426afb4f3fc798d31dcb0a8
daniel araujo <daniellealuau@gmail.com>
2024-03-03 22:07:47 -0300

    3

--- 01/01.js
+++ 01/01.js
@@ -1,6 +1,5 @@
 import inquirer from "inquirer";
 
-
 class Vertice {
   #x; // atributo privado para armazenar a coordenada x
   #y; // atributo privado para armazenar a coordenada y
@@ -22,10 +21,12 @@ class Vertice {
   }
 
   // método para calcular a distância euclidiana entre dois vértices
-  distancia(vertice2) {
-    const distX = this.#x - vertice2.getX();
-    const distY = this.#y - vertice2.getY();
-    return Math.sqrt(distX ** 2 + distY ** 2);
+  distancia(vertice2, vertice3) {
+    const distX2 = this.#x - vertice2.getX();
+    const distY2 = this.#y - vertice2.getY();
+    const distX3 = this.#x - vertice3.getX();
+    const distY3 = this.#y - vertice3.getY();
+    return Math.sqrt(distX2 ** 2 + distY2 ** 2 + distX3 ** 2 + distY3 ** 2);
   }
 
   // método para mover o vértice para novas coordenadas
@@ -35,8 +36,8 @@ class Vertice {
   }
 
   // método para verificar se dois vértices são iguais
-  equals(vertice2) {
-    if (this.#x === vertice2.#x && this.#y === vertice2.#y) {
+  equals(vertice2, vertice3) {
+    if (this.#x === vertice2.getX() && this.#x === vertice3.getX() && this.#y === vertice2.getY() && this.#y === vertice3.getY()) {
       return "sim";
     } else {
       return "nao";
@@ -48,47 +49,26 @@ class Vertice {
 function criarVertice() {
   inquirer
     .prompt([
-      {
-        type: "input",
-        name: "x1",
-        message: "digite a coordenada x do primeiro vértice: ",
-      },
-      {
-        type: "input",
-        name: "y1",
-        message: "digite a coordenada y do primeiro vértice: ",
-      },
-      {
-        type: "input",
-        name: "x2",
-        message: "digite a coordenada x do segundo vértice: ",
-      },
-      {
-        type: "input",
-        name: "y2",
-        message: "digite a coordenada y do segundo vértice: ",
-      },
+      { type: "input", name: "x1", message: "digite a coordenada x do primeiro vértice: " },
+      { type: "input", name: "y1", message: "digite a coordenada y do primeiro vértice: " },
+      { type: "input", name: "x2", message: "digite a coordenada x do segundo vértice: " },
+      { type: "input", name: "y2", message: "digite a coordenada y do segundo vértice: " },
+      { type: "input", name: "x3", message: "digite a coordenada x do terceiro vértice: " },
+      { type: "input", name: "y3", message: "digite a coordenada y do terceiro vértice: " },
     ])
     .then((answers) => {
       // cria instâncias da classe Vertice com base nas respostas do usuário
-      const primeiroVertice = new Vertice(
-        Number(answers.x1),
-        Number(answers.y1)
-      );
-      const segundoVertice = new Vertice(
-        Number(answers.x2),
-        Number(answers.y2)
-      );
-      const distanciaEntreVertices = primeiroVertice.distancia(segundoVertice);
+      const primeiroVertice = new Vertice(Number(answers.x1), Number(answers.y1));
+      const segundoVertice = new Vertice(Number(answers.x2), Number(answers.y2));
+      const terceiroVertice = new Vertice(Number(answers.x3), Number(answers.y3));
+      const distanciaEntreVertices = primeiroVertice.distancia(segundoVertice, terceiroVertice);
 
-      console.log(`vertice1 criado: 
-        x: ${primeiroVertice.getX()} y: ${primeiroVertice.getY()}`);
-      console.log(`vertice2 criado: 
-        x: ${segundoVertice.getX()} y: ${segundoVertice.getY()}`);
-      console.log(`distancia entre os vertices: ${distanciaEntreVertices}`);
-      console.log(
-        `são vertices iguais? ${primeiroVertice.equals(segundoVertice)}.`
-      );
+      // imprime as informações sobre os vértices e a distância entre eles
+      console.log(`primeiro vértice criado: x: ${primeiroVertice.getX()} y: ${primeiroVertice.getY()}`);
+      console.log(`segundo vértice criado: x: ${segundoVertice.getX()} y: ${segundoVertice.getY()}`);
+      console.log(`terceiro vértice criado: x: ${terceiroVertice.getX()} y: ${terceiroVertice.getY()}`);
+      console.log(`distância entre os vértices: ${distanciaEntreVertices}`);
+      console.log(`são vértices iguais? ${primeiroVertice.equals(segundoVertice, terceiroVertice)}.`);
     });
 }
 
