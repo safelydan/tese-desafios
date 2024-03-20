0278a386ee337fc286ec93a923a9ccc3b230dec7
danieluau <daniellealuau@gmail.com>
2024-03-20 16:19:42 -0300

    ok

--- desafio 1.2/controllers/ConsultaController.js
+++ desafio 1.2/controllers/ConsultaController.js
@@ -9,9 +9,9 @@ class ConsultaController {
     async agendarConsultaInterativa() {
         const dadosConsulta = await this.obterDadosConsulta();
         const consulta = new Consulta(dadosConsulta.paciente, dadosConsulta.data, dadosConsulta.horaInicial, dadosConsulta.horaFinal);
-        console.log("Dados da consulta:", dadosConsulta); // Adicionando console.log para exibir os dados de entrada
-    
-        // Verificações
+        console.log("Dados da consulta:", dadosConsulta);
+
+
         if (!this.validarFormatoCPF(dadosConsulta.paciente)) {
             console.log("Erro: Formato de CPF inválido.");
             return;
@@ -108,44 +108,37 @@ class ConsultaController {
         const horaRegex = /^\d{4}$/;
         return horaRegex.test(hora);
     }
-    
+
     validarDataFutura(data) {
         const currentDate = new Date();
 
-    
-        // Dividir a string da data em dia, mês e ano
         const [dia, mes, ano] = data.split('/');
-    
-        // Criar uma nova string no formato AAAA-MM-DD
         const dataFormatada = `${ano}-${mes}-${dia}`;
-
-    
-        // Criar o objeto Date com a data formatada
         const consultaDate = new Date(dataFormatada);
 
         return consultaDate.getTime() > currentDate.getTime();
     }
-    
-    
+
+
 
     validarHoraInicialAtual(data, horaInicial) {
         const currentDate = new Date();
 
-        
+
         const [dia, mes, ano] = data.split('/');
         const dataFormatada = `${ano}-${mes}-${dia}`;
 
-        
+
         const consultaDate = new Date(dataFormatada);
 
-        
+
         const consultaStartTime = new Date(`${dataFormatada}T${horaInicial.slice(0, 2)}:${horaInicial.slice(2)}:00`);
-        
+
         if (isNaN(consultaDate.getTime())) {
             console.log("Data da consulta inválida.");
             return false;
         }
-        
+
         if (consultaDate.getTime() > currentDate.getTime()) {
             console.log("A data da consulta está no futuro.");
             return true;
@@ -163,11 +156,11 @@ class ConsultaController {
             return false;
         }
     }
-    
+
     validarHoraFinalMaior(horaInicial, horaFinal) {
         return parseInt(horaFinal) > parseInt(horaInicial);
     }
-    
+
 
     pacientePossuiConsultaFutura(paciente) {
         const pacienteConsulta = this.consultas.find(consulta => consulta.paciente === paciente);
@@ -194,105 +187,116 @@ class ConsultaController {
     }
 
     validarHorarioFuncionamento(data, horaInicial, horaFinal) {
-        // Verificar se os horários de abertura e fechamento estão no formato correto
+
         if (!this.validarFormatoData(data)) {
             console.log("Erro: Formato de data inválido. Use DD/MM/AAAA.");
             return false;
         }
-    
-        // Verificar se os horários de abertura e fechamento estão no formato correto
+
         if (!this.validarFormatoHora(horaInicial) || !this.validarFormatoHora(horaFinal)) {
             console.log("Erro: Formato de hora inválido. Use HHMM.");
             return false;
         }
-    
-        console.log("Data:", data); // Verificar o formato da data aqui
-    
-        // Dividir a data em dia, mês e ano
+
+        console.log("Data:", data);
+
         const [dia, mes, ano] = data.split('/');
-        
-        // Criar objetos Date para a data, hora de abertura e hora de fechamento
+
         const dataFormatada = new Date(`${ano}-${mes}-${dia}`);
         const horarioAbertura = new Date(dataFormatada);
-        horarioAbertura.setHours(8, 0, 0); // Define a hora de abertura para 08:00:00
+        horarioAbertura.setHours(8, 0, 0);
         const horarioFechamento = new Date(dataFormatada);
-        horarioFechamento.setHours(19, 0, 0); // Define a hora de fechamento para 19:00:00
+        horarioFechamento.setHours(19, 0, 0);
         const consultaStartTime = new Date(dataFormatada);
         consultaStartTime.setHours(parseInt(horaInicial.slice(0, 2)), parseInt(horaInicial.slice(2)), 0);
         const consultaEndTime = new Date(dataFormatada);
         consultaEndTime.setHours(parseInt(horaFinal.slice(0, 2)), parseInt(horaFinal.slice(2)), 0);
-        
+
         const dentroDoHorario = consultaStartTime >= horarioAbertura && consultaEndTime <= horarioFechamento;
-        
+
         if (dentroDoHorario) {
             console.log("A consulta está dentro do horário de funcionamento.");
         } else {
             console.log("A consulta está fora do horário de funcionamento.");
         }
-        
+
         return dentroDoHorario;
     }
+
     
-    
-    
-    
-        async cancelarConsultaInterativa() {
-            const dadosCancelamento = await this.obterDadosCancelamento();
-    
-            // Verificar se a consulta existe
-            const consultaIndex = this.consultas.findIndex(consulta => {
-                return consulta.paciente === dadosCancelamento.paciente &&
-                       consulta.data === dadosCancelamento.data &&
-                       consulta.horaInicial === dadosCancelamento.horaInicial;
-            });
-    
-            if (consultaIndex === -1) {
-                console.log("Erro: Consulta não encontrada para cancelamento.");
-                return;
-            }
-    
-            // Verificar se a consulta é no futuro
-            const consulta = this.consultas[consultaIndex];
-            const consultaDate = new Date(`${consulta.data} ${consulta.horaInicial.slice(0, 2)}:${consulta.horaInicial.slice(2)}:00`);
-            const currentDate = new Date();
-            if (consultaDate < currentDate) {
-                console.log("Erro: Só é possível cancelar uma consulta futura.");
-                return;
-            }
-    
-            // Remover consulta
-            this.consultas.splice(consultaIndex, 1);
-            console.log("Consulta cancelada com sucesso.");
-        }
-    
-        async obterDadosCancelamento() {
-            const perguntas = [
-                {
-                    type: 'input',
-                    name: 'paciente',
-                    message: 'CPF do paciente:'
-                },
-                {
-                    type: 'input',
-                    name: 'data',
-                    message: 'Data da consulta (DD/MM/AAAA):'
-                },
-                {
-                    type: 'input',
-                    name: 'horaInicial',
-                    message: 'Hora inicial da consulta (HHMM):'
-                }
-            ];
-    
-            const respostas = await inquirer.prompt(perguntas);
-            return respostas;
-            
+
+    async cancelarConsultaInterativa() {
+        const dadosCancelamento = await this.obterDadosCancelamento();
+
+
+        const consultaIndex = this.consultas.findIndex(consulta => {
+            return consulta.paciente === dadosCancelamento.paciente &&
+                consulta.data === dadosCancelamento.data &&
+                consulta.horaInicial === dadosCancelamento.horaInicial;
+        });
+
+        if (consultaIndex === -1) {
+            console.log("Erro: Consulta não encontrada para cancelamento.");
+            return;
         }
 
-        
+
+        const consulta = this.consultas[consultaIndex];
+        const consultaDate = new Date(`${consulta.data} ${consulta.horaInicial.slice(0, 2)}:${consulta.horaInicial.slice(2)}:00`);
+        const currentDate = new Date();
+        if (consultaDate < currentDate) {
+            console.log("Erro: Só é possível cancelar uma consulta futura.");
+            return;
+        }
+
+        this.consultas.splice(consultaIndex, 1);
+        console.log("Consulta cancelada com sucesso.");
+    }
+
+    async obterDadosCancelamento() {
+        const perguntas = [
+            {
+                type: 'input',
+                name: 'paciente',
+                message: 'CPF do paciente:'
+            },
+            {
+                type: 'input',
+                name: 'data',
+                message: 'Data da consulta (DD/MM/AAAA):'
+            },
+            {
+                type: 'input',
+                name: 'horaInicial',
+                message: 'Hora inicial da consulta (HHMM):'
+            }
+        ];
+
+        const respostas = await inquirer.prompt(perguntas);
+        return respostas;
+
+    }
+
+        listarConsultas() {
+        if (this.consultas.length === 0) {
+            console.log("Não há consultas agendadas.");
+        } else {
+            console.log("Consultas agendadas:");
+            this.consultas.forEach((consulta, index) => {
+                console.log(`Consulta ${index + 1}:`);
+                console.log(`Paciente: ${consulta.paciente}`);
+                console.log(`Data: ${consulta.data}`);
+                console.log(`Hora Inicial: ${consulta.horaInicial}`);
+                console.log(`Hora Final: ${consulta.horaFinal}`);
+                console.log("---------------------------------------");
+            });
+        }
+    }
+
+
 }
-    
-    
+
+
 
 
 export default ConsultaController;
--- desafio 1.2/view/main.js
+++ desafio 1.2/view/main.js
@@ -1,9 +1,11 @@
 import inquirer from "inquirer";
 import CadastroPaciente from "../controllers/CadastroPaciente.js";
 import { cadastrarNovoPaciente } from "../controllers/CadastroPaciente.js";
+import Agenda from "../controllers/ConsultaView.js"
 import Paciente from "../models/Paciente.js";
 
 const cadastro = new CadastroPaciente();
+const agenda = new Agenda();
 
 async function menuPrincipal() {
   while (true) {
@@ -18,7 +20,7 @@ async function menuPrincipal() {
       case "1-Cadastro de pacientes":      await menuCadastro(); 
         break;
       case "2-Agenda":
-        cadastro.listarPacientes();
+        agenda.iniciar();
         break;
       case "3-Fim": 
         console.log("Encerrando o programa...");
@@ -67,6 +69,41 @@ async function menuCadastro() {
         break;
       case "5-Voltar p/ menu principal": 
         return;
+    }
+  }
+}
+
+async function menuAgenda() {
+  while (true) {
+    console.log("\n=== Menu ===");
+
+    const { opcao } = await inquirer.prompt({
+      type: 'list',
+      name: 'opcao',
+      message: 'Escolha uma opção:',
+      choices: [
+        { name: 'Agendar consulta', value: '1' },
+        { name: 'Cancelar consulta', value: '2' },
+        { name: 'Listar agenda', value: '3' }, // Nova opção para listar consultas
+        { name: 'Voltar p/ menu principal', value: '4' }
+      ]
+    });
+
+    switch (opcao) {
+      case '1':
+        await this.consultaController.agendarConsultaInterativa();
+        break;
+      case '2':
+        await this.consultaController.cancelarConsultaInterativa();
+        break;
+      case '3':
+        this.consultaController.listarConsultas(); // Chamada para o método listarConsultas
+        break;
+      case '4':
+        console.log("Saindo...");
+        return;
+      default:
+        console.log("Opção inválida.");
     }
   }
 }
